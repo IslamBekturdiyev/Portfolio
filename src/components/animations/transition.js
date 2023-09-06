cd82ee7b417ec175da2a0df6fdf4d95ae1b1eaf4
Islambek <islambey0443366@gmail.com>
2023-09-06 22:18:05 +0500

    fixed

--- src/components/animations/transition.js
+++ src/components/animations/transition.js
@@ -1,15 +1,16 @@
-'use client'; 
+'use client';
+import React from "react"; // Import React
 import { motion } from "framer-motion";
 
 const transition = (OgComponent) => {
-  return () => (
+  const TransitionComponent = () => (
     <>
       <OgComponent />
       <motion.div
         className="slide-in"
-        initial={{ scaleY: 0}}
-        animate={{ scaleY: 0}}
-        exit={{ scaleY: 1}}
+        initial={{ scaleY: 0 }}
+        animate={{ scaleY: 0 }}
+        exit={{ scaleY: 1 }}
         transition={{ duration: 1.3, ease: [0.22, 1, 0.36, 1] }}
       ></motion.div>
       <motion.div
@@ -17,10 +18,20 @@ const transition = (OgComponent) => {
         initial={{ scaleY: 1 }}
         animate={{ scaleY: 0 }}
         exit={{ scaleY: 0 }}
-        transition={{duration: 1.3, ease: [0.22, 1, 0.36, 1] }}
+        transition={{ duration: 1.3, ease: [0.22, 1, 0.36, 1] }}
       ></motion.div>
     </>
   );
+
+  // Assign a display name to your component
+  TransitionComponent.displayName = `Transition(${getDisplayName(OgComponent)})`;
+
+  return TransitionComponent;
 };
+
+// A utility function to get the display name of a component
+function getDisplayName(WrappedComponent) {
+  return WrappedComponent.displayName || WrappedComponent.name || "Component";
+}
 
 export default transition;
